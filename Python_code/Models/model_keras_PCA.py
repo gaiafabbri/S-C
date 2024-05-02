40b6828107e4c897ae25684a8c402fb81ffb16d3
gaiafabbri <133896928+gaiafabbri@users.noreply.github.com>
2024-05-02 15:35:26 +0200

    Update model_keras_PCA.py

--- Python_code/Models/model_keras_PCA.py
+++ Python_code/Models/model_keras_PCA.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Input
-from tensorflow.keras.optimizers import SGD, Adam, RMSprop, Adagrad, Adadelta
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop, Adagrad, Adadelta #other choices
 
 '''----------------- MODEL DEFINITION------------------'''
 ''' The model chosen is a 1D CNN since the images are now represented by one-dimensional arrays after being flattened and reduced by the PCA process
@@ -13,20 +13,17 @@ from tensorflow.keras.optimizers import SGD, Adam, RMSprop, Adagrad, Adadelta
 
 def keras_PCA(n_principal_components):
     # Definition and compilation of the model
-    model = Sequential([ ### nota che si dovrebbe cambiare anche la kernel size, strides, etc... da capire come tunare i parametri
+    model = Sequential([
         Input(shape=(n_principal_components, 1)),
         Conv1D(10, kernel_size=3, activation='relu', padding='same'),
         MaxPooling1D(pool_size=2, strides=2),
         Flatten(),
         Dense(256, activation='relu'),
-        Dense(1, activation='sigmoid')  # Utilizzo di un solo neurone con attivazione sigmoid
+        Dense(1, activation='sigmoid')  
     ]) #vedere sigmoid o softmax
 
     model.compile(optimizer='adam',
-              loss='binary_crossentropy',  # Utilizzo di binary_crossentropy per la classificazione binaria
+              loss='binary_crossentropy',  
               metrics=['accuracy'])
-    '''model.compile(optimizer=RMSprop(),
-              loss='binary_crossentropy',  # Utilizzo di binary_crossentropy per la classificazione binaria
-              metrics=['accuracy'])'''
     model.summary()
     return model
