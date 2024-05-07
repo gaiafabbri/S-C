810e87bcb80fa9e4ec626e1337b20bded33ab57e
gaiafabbri <133896928+gaiafabbri@users.noreply.github.com>
2024-05-07 07:32:50 +0200

    Update model_keras_PCA.py

--- Python_code/Models/model_keras_PCA.py
+++ Python_code/Models/model_keras_PCA.py
@@ -6,9 +6,10 @@ from tensorflow.keras.optimizers import SGD, Adam, RMSprop, Adagrad, Adadelta #o
 ''' The model chosen is a 1D CNN since the images are now represented by one-dimensional arrays after being flattened and reduced by the PCA process
 1) The activation function relu is chosen
 2) The input shape is given by (number of principal components, batch size) where the number of principal components is previously computed
-3)the MaxPooling layer reduces the input spatial dimension
-4) the Flatten layer ensures that the output of the previous steps is a one-dimensional array to be passed to the following dense layer with a relu activation function
-5)the output layer uses a sigmoid function to generate the probability distribution for an image to belong to the singal or background classes; this is common for binary classification problem
+3) A convolutional layer is inserted with ReLU activation function
+4)the MaxPooling layer reduces the input spatial dimension
+5) the Flatten layer ensures that the output of the previous steps is a one-dimensional array to be passed to the following dense layer with a relu activation function
+6)the output layer uses a sigmoid function to generate the probability distribution for an image to belong to the singal or background classes; this is common for binary classification problem
  '''
 
 def keras_PCA(n_principal_components):
