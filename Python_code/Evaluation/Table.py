3c0d243ef84ff341b0ad8440738158a311d916f8
gaiafabbri <133896928+gaiafabbri@users.noreply.github.com>
2024-05-12 17:00:29 +0200

    the best results are coloured in yellow in the final table

--- Python_code/Evaluation/Table.py
+++ Python_code/Evaluation/Table.py
@@ -11,7 +11,8 @@ import pandas as pd
  5) Then, it iterates through the provided model results: they include the model name, the accuracy, the precision, the f1-score and the training time
  6) Adds them to the DataFrame.
  7) Draws the table: it relies on Pillow to create a void images with specified dimensions (img_width e img_height) and then draws the table (and its content) wiht ImageDraw
- 8) Finally, it saves the table as a PNG image and prints a confirmation message.
+ 8) The best results are found and the corresponding cell is coloured in yellow
+ 9) Finally, it saves the table as a PNG image and prints a confirmation message.
 '''
 
 def save_results_table(model_results, results_df):
@@ -50,11 +51,26 @@ def save_results_table(model_results, results_df):
         draw.rectangle([i * cell_width, 0, (i + 1) * cell_width, cell_height], outline='black', fill='lightgrey')
         draw.text((i * cell_width + 5, 5), column, fill='black', font=font)
 
-    # Draw table content
+  # Draw table content and find best results
+    best_accuracy_index = results_df['Accuracy'].idxmax()
+    best_precision_index = results_df['Precision'].idxmax()
+    best_f1_index = results_df['F1 Score'].idxmax()
+    fastest_time_index = results_df['Training Time (s)'].idxmin()
+
     for i, row in enumerate(results_df.values):
         for j, cell in enumerate(row):
+            cell_color = 'white'  # Default color
+            if i == best_accuracy_index and j == results_df.columns.get_loc('Accuracy'):
+                cell_color = 'yellow'  # Yellow for best accuracy
+            elif i == best_precision_index and j == results_df.columns.get_loc('Precision'):
+                cell_color = 'yellow'  # Yellow for best precision
+            elif i == best_f1_index and j == results_df.columns.get_loc('F1 Score'):
+                cell_color = 'yellow'  # Yellow for best F1 Score
+            elif i == fastest_time_index and j == results_df.columns.get_loc('Training Time (s)'):
+                cell_color = 'yellow'  # Yellow for fastest training time
+
             draw.rectangle([j * cell_width, (i + 1) * cell_height, (j + 1) * cell_width, (i + 2) * cell_height],
-                           outline='black', fill='white')
+                           outline='black', fill=cell_color)
             draw.text((j * cell_width + 5, (i + 1) * cell_height + 5), str(cell), fill='black', font=font)
 
     # Save the image
